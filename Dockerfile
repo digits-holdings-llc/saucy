78f9ec813f5347f441c44b99917cfcc19c6b282d
Aram Bayadyan <aram.bv@gmail.com>
2019-12-24 14:24:18 +0700

    refactoring, added Dockerfile, updated to greenbot-sdk 1.0.13

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:13.3.0-alpine3.10
+
+RUN apk add --no-cache tini
+
+RUN mkdir /web
+WORKDIR /web
+COPY package.json package-lock.json /web/
+
+RUN cd /web && npm install
+COPY . /web
+RUN mv /web/config.yaml.template /web/config.yaml
+
+ENTRYPOINT [ "tini","--" ]
+CMD ["node","/web/index.js"]
--- index.js
+++ index.js
@@ -1,114 +1,102 @@
-const express = require('express')
-const app = express()
-var http = require('http').createServer(app);
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
 
-const port = process.env.WEB_PORT || 80
-var MongoClient = require('mongodb').MongoClient
-const { GraphQLClient } = require('graphql-request')
-const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/saucy'
-const parts = mongoURL.split("/")
-const DB_NAME = parts[parts.length - 1]
-var contactTimeout
-var botSDK = require('greenbot-sdk')
-var faker = require('faker');
-
+const port = process.env.WEB_PORT || 80;
+const { GraphQLClient } = require('graphql-request');
+const { init, log } = require('greenbot-sdk');
+const faker = require('faker');
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 app.use(express.urlencoded());
-app.engine('pug', require('pug').__express)
-app.set('view engine', 'pug')
-app.set('views', './views')
-app.use(express.static('public'))
-botSDK.init(app, http)
-
-
+app.engine('pug', require('pug').__express);
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.static('public'));
+init(app, http);
 
 const sauce = [
-  "What blind man dressed you?",
-  "More of your conversation would infect my brain.",
-  "He has not so much brain as ear-wax.",
-  "Thou cream faced loon!",
-  "If I agreed with you we’d both be wrong."
-]
+  'What blind man dressed you?',
+  'More of your conversation would infect my brain.',
+  'He has not so much brain as ear-wax.',
+  'Thou cream faced loon!',
+  'If I agreed with you we’d both be wrong.'
+];
 
 // Access the parse results as request.body
-app.post('/', async function(request, response){
-  var inboundMsg = request.body;
+app.post('/', async function(request, response) {
+  const inboundMsg = request.body;
 
   // If this is a session end event, ignore
   if (inboundMsg.type == 'session_end' || inboundMsg.type == 'new_session') {
-    response.send({})
+    response.send({});
     return;
   }
   if (!inboundMsg.msg) {
-    response.send({})
+    response.send({});
     return;
   }
-  if (request.body.msg.direction == "egress") {
-    if (inboundMsg.msg.txt.toUpperCase().includes("QUIT")) {
-      botSDK.log("Quitting")
+  if (request.body.msg.direction == 'egress') {
+    if (inboundMsg.msg.txt.toUpperCase().includes('QUIT')) {
+      log('Quitting');
       response.send({
         commands: {
-          "end_session": true
+          end_session: true
         }
-      })
+      });
     } else {
-      response.send({})
+      response.send({});
     }
     return;
   }
 
-  var jsonResponse = { }
-  var trigger = request.config.trigger.toUpperCase()
-  botSDK.log("New message : ", inboundMsg.msg.src, ":", inboundMsg.msg.txt)
+  const jsonResponse = {};
+  const trigger = request.config.trigger.toUpperCase();
+  log('New message : ', inboundMsg.msg.src, ':', inboundMsg.msg.txt);
   if (trigger && inboundMsg.msg.txt.toUpperCase().includes(trigger)) {
-    var respText = request.config.response_msg || "No respnse_msg found in the configuration"
-    if (request.config.fake_language.toUpperCase().trim() == "TRUE") {
-      respText = faker.lorem.sentences(4)
+    const respText = request.config.response_msg || 'No respnse_msg found in the configuration';
+    if (request.config.fake_language.toUpperCase().trim() == 'TRUE') {
+      respText = faker.lorem.sentences(4);
     }
     jsonResponse = {
-      messages: [
-        { txt: respText}
-      ]
-    }
-    const extraMsgCount = parseInt(request.config.extra_sauce)
-    for (count=0; count < extraMsgCount; count++) {
-      jsonResponse.messages.push({txt: sauce[Math.floor(Math.random()*sauce.length)]})
+      messages: [{ txt: respText }]
+    };
+    const extraMsgCount = parseInt(request.config.extra_sauce);
+    for (count = 0; count < extraMsgCount; count++) {
+      jsonResponse.messages.push({ txt: sauce[Math.floor(Math.random() * sauce.length)] });
     }
   }
 
-  if (request.config.tags != "") {
-    jsonResponse.agentTags = [ request.config.tags ]
+  if (request.config.tags != '') {
+    jsonResponse.agentTags = [request.config.tags];
   }
-  if (request.config.whisper.toUpperCase().trim() == "TRUE") {
-    jsonResponse.whispers = [{txt: faker.lorem.sentences(4) }]
+  if (request.config.whisper.toUpperCase().trim() == 'TRUE') {
+    jsonResponse.whispers = [{ txt: faker.lorem.sentences(4) }];
   }
 
-  if (inboundMsg.msg.txt.toUpperCase().includes("QUIT")) {
+  if (inboundMsg.msg.txt.toUpperCase().includes('QUIT')) {
     jsonResponse.commands = {
-      "end_session": true
-    }
-    botSDK.log("Quitting")
+      end_session: true
+    };
+    log('Quitting');
   }
 
-  if (request.config.transfer && inboundMsg.msg.txt.toUpperCase().includes("TRANSFER")) {
-    botSDK.log("Transferring to ", request.config.transfer)
+  if (request.config.transfer && inboundMsg.msg.txt.toUpperCase().includes('TRANSFER')) {
+    log('Transferring to ', request.config.transfer);
     if (jsonResponse.commands) {
-      jsonResponse.commands.transfer = request.config.transfer
+      jsonResponse.commands.transfer = request.config.transfer;
     } else {
       jsonResponse.commands = {
-        "transfer": request.config.transfer
-      }
+        transfer: request.config.transfer
+      };
     }
   }
-  response.send(jsonResponse)
-})
-
-
+  response.send(jsonResponse);
+});
 
 app.get('/', async function(request, response) {
-  response.render('index', { config: request.config })
-})
+  response.render('index', { config: request.config });
+});
 
-http.listen(port, () => botSDK.log(`Automation running on ${port}!`))
+http.listen(port, () => log(`Automation running on ${port}!`));
